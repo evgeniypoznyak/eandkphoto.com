38d4797cdeb5ceb85f214a81359fa388cdc5c75c
Evgeniy Poznyak <ek35mm@gmail.com>
2017-11-26 23:40:07 -0600

    Revert "EK-004: Headers (with error)"
    This reverts commit 9c4609d

--- src/app/core/services/http/laravel.service.ts
+++ src/app/core/services/http/laravel.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { OptionsService } from '../options.service';
 
 @Injectable()
 export class LaravelService {
+  api = this._options.api;
+  private httpOptions = {
+    headers: new HttpHeaders(
+      {
+        'Content-Type': 'application/json',
+        'X-Requested-With': ['XMLHttpRequest']
+      }
+      // можно массив передавать, только надо настроить Cors.php
+    )
+  }
 
-  constructor() { }
+  constructor(private _http: HttpClient, private _options: OptionsService) { }
+
+  getOneSlider(id: number) {
+    return this._http.get<any[]>(this.api.sliderApi + id, this.httpOptions)
+      .map(response => {
+      return response
+      })
+      .do(data => {
+      return data
+      });
+  }
 
 }

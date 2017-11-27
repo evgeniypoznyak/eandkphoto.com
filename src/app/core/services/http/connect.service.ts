c74255a3e539b70ef59d45f00cedabd8ec639c46
Evgeniy Poznyak <ek35mm@gmail.com>
2017-11-26 23:40:19 -0600

    Revert "EK-004: Firebase and Laravel (Headers need to work)"
    This reverts commit 109948d

--- src/app/core/services/http/connect.service.ts
+++ src/app/core/services/http/connect.service.ts
@@ -1,24 +1,8 @@
 import { Injectable } from '@angular/core';
-import { OptionsService } from '../options.service';
-import { LaravelService } from './laravel.service';
-import { FirebaseService } from './firebase.service';
 
 @Injectable()
 export class ConnectService {
 
-  constructor(private optionsService: OptionsService,
-              private laravelService: LaravelService,
-              private firebaseService: FirebaseService) { }
-
-
-  getOneSliderItem(id: number) {
-    switch (this.optionsService.base) {
-      case 'laravel':
-        return this.laravelService.getOneSlider(+id);
-        break;
-      case 'firebase':
-        break;
-    }
-  }
+  constructor() { }
 
 }

b021175fd2e6cf93369ecb80adeb0238fc945b63
Evgeniy Poznyak <ek35mm@gmail.com>
2017-11-26 23:40:36 -0600

    Revert "EK-004: Headers (with error)"
    This reverts commit 9c4609d

--- src/app/core/services/http/connect.service.ts
+++ src/app/core/services/http/connect.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { OptionsService } from '../options.service';
+import { LaravelService } from './laravel.service';
+import { FirebaseService } from './firebase.service';
 
 @Injectable()
 export class ConnectService {
 
-  constructor() { }
+  constructor(private optionsService: OptionsService,
+              private laravelService: LaravelService,
+              private firebaseService: FirebaseService) { }
+
+
+  getOneSliderItem(id: number) {
+    switch (this.optionsService.base) {
+      case 'laravel':
+        return this.laravelService.getOneSlider(+id);
+        break;
+      case 'firebase':
+        break;
+    }
+  }
 
 }

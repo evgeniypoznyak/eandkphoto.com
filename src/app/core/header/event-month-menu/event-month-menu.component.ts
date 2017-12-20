c93566266df017e59e56e2d56f5efc430e5e4da8
Evgeniy Poznyak <ek35mm@gmail.com>
2017-12-20 07:20:07 -0600

    Years in menu and data in the month component

--- src/app/core/header/event-month-menu/event-month-menu.component.ts
+++ src/app/core/header/event-month-menu/event-month-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-event-month-menu',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventMonthMenuComponent implements OnInit {
 
+  @Input() yearStr: any;
+  @Input() events: any;
+  @Input() yearArr: any;
   constructor() { }
 
   ngOnInit() {
+    console.log(this.events);
+    console.log(this.yearStr);
+    console.log(this.yearArr);
   }
 
 }
